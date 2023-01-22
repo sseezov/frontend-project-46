c13c76abccb18a93eff0528ce7992d40d7513b63
sseezov <sseezov@mail.ru>
2023-01-22 17:49:45 +0700

    3 step done

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
+install:
+	npm ci
+
+lint:
+	npx eslint .
+
+lintfix:
+	npx eslint . --fix
+
 test:
 	node bin/gendiff __fixtures__/file1.json __fixtures__/file2.json
+
--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
+/* eslint-disable no-restricted-syntax */
 import path from 'node:path';
 import { readFileSync } from 'node:fs';
 
 const getAbslPath = (fileName) => path.resolve(process.cwd(), fileName);
-const hasProp = (array, prop) => array.indexOf(prop)>=0;
+const hasProp = (array, prop) => array.indexOf(prop) >= 0;
 
 const getDataDifferences = (data1, data2) => {
   const data1Keys = Object.keys(data1);
   const data2Keys = Object.keys(data2);
   const allKeys = [...data1Keys, ...data2Keys];
   const sortedAllKeys = allKeys.sort();
-  let result = ['{'];
+  const result = ['{'];
 
-  for (let key in sortedAllKeys){
-    if (data1[sortedAllKeys[key]] === data2[sortedAllKeys[key]] && !hasProp(result, `    ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`)){
-      result.push(`\n`)
-      result.push(`    ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`)
-    }
-    else if (data1[sortedAllKeys[key]] !== data2[sortedAllKeys[key]]){
-      if (Object.hasOwn(data1, sortedAllKeys[key]) && !Object.hasOwn(data2, sortedAllKeys[key])){
-        result.push(`\n`)
-        result.push(`  - ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`)
-      }
-      else if (Object.hasOwn(data1, sortedAllKeys[key]) && Object.hasOwn(data2, sortedAllKeys[key])
-        && !hasProp(result, `  - ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`)){
-        result.push(`\n`)
-        result.push(`  - ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`)
-        result.push(`\n`)
-        result.push(`  + ${sortedAllKeys[key]}: ${data2[sortedAllKeys[key]]}`)
-      }
-      else if (!Object.hasOwn(data1, sortedAllKeys[key]) && Object.hasOwn(data2, sortedAllKeys[key])){
-        result.push(`\n`)
-        result.push(`  + ${sortedAllKeys[key]}: ${data2[sortedAllKeys[key]]}`)
+  for (const key in sortedAllKeys) {
+    if (data1[sortedAllKeys[key]] === data2[sortedAllKeys[key]] && !hasProp(result, `    ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`)) {
+      result.push('\n');
+      result.push(`    ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`);
+    } else if (data1[sortedAllKeys[key]] !== data2[sortedAllKeys[key]]) {
+      if (Object.hasOwn(data1, sortedAllKeys[key]) && !Object.hasOwn(data2, sortedAllKeys[key])) {
+        result.push('\n');
+        result.push(`  - ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`);
+      } else if (Object.hasOwn(data1, sortedAllKeys[key])
+      && Object.hasOwn(data2, sortedAllKeys[key])
+      && !hasProp(result, `  - ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`)) {
+        result.push('\n');
+        result.push(`  - ${sortedAllKeys[key]}: ${data1[sortedAllKeys[key]]}`);
+        result.push('\n');
+        result.push(`  + ${sortedAllKeys[key]}: ${data2[sortedAllKeys[key]]}`);
+      } else if (!Object.hasOwn(data1, sortedAllKeys[key])
+      && Object.hasOwn(data2, sortedAllKeys[key])) {
+        result.push('\n');
+        result.push(`  + ${sortedAllKeys[key]}: ${data2[sortedAllKeys[key]]}`);
       }
     }
   }
-  result.push('\n' + '}');
+  result.push('\n}');
   return result.join('');
 };
 
-export const genDiff = (file1Name, file2Name) => {
+export default function genDiff(file1Name, file2Name) {
   const file1Path = getAbslPath(file1Name);
   const file1Data = readFileSync(file1Path, 'utf-8');
   const file1ParsedData = JSON.parse(file1Data);
@@ -49,4 +49,4 @@ export const genDiff = (file1Name, file2Name) => {
 
   const filesDifferences = getDataDifferences(file1ParsedData, file2ParsedData);
   return filesDifferences;
-};
+}
