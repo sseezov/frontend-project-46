f27b98b7791f54b7ea25ea0acb4484345d6ca131
sseezov <sseezov@mail.ru>
2023-01-30 21:59:12 +0700

    crlf to lf

--- src/parsers.js
+++ src/parsers.js
@@ -3,12 +3,13 @@ import { load as YMLParse } from 'js-yaml';
 import { getData, getFormat } from './utils.js';
 
 const parser = (file) => {
+  const data = getData(file);
   const extension = getFormat(file);
-  if (extension === 'yaml' || extension === 'yml') {
-    return YMLParse(getData(file));
-  }
 
-  return JSON.parse(getData(file));
+  if (extension === 'yaml' || extension === 'yml') {
+    return YMLParse(data);
+  }
+  return JSON.parse(data);
 };
 
 export default parser;
