2cc14cca3c010d1983862c4a72133d6540deaa12
sseezov <sseezov@mail.ru>
2023-03-15 17:20:31 +0700

    3 step done

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	npm ci
+
+run:
+	node bin/gendiff.js file1.json file2.json
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,16 @@
 import { Command } from 'commander';
+import showDiff from '../src/index.js';
 const program = new Command();
 
+
 program
-  .name('gediff-js')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.0.1');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action(showDiff)
 
 program.parse();
