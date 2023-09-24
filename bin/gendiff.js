cd80abbd9c7a6a1338338384068e6f6806d8314b
sseezov <sseezov@mail.ru>
2023-09-24 12:56:58 +0700

    small fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,8 +8,7 @@ program
   .name('gendiff')
   .usage('[options] < filepath1 > <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-s, --format [type]', 'stylish')
-  .option('-d, --letter [type]')
+  .option('-f, --format [type]', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const result = showDiff(filepath1, filepath2, program.opts().format);
